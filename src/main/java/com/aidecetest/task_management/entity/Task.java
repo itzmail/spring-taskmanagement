f15ad8b8e7f86f584f3dc27f6550afc59eb25fa1
ismailalam <ismailnuralam@gmail.com>
2025-11-16 14:49:13 +0700

    Success Running server and make entity Task

---
+++ src/main/java/com/aidecetest/task_management/entity/Task.java
@@ -0,0 +1,28 @@
+package com.aidecetest.task_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(length = 1000)
+  private String description;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Status status;
+}
